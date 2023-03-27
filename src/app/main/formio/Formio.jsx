4b05e1b898b020959d66b6554d21bfd72da0273c
Yan Latyshev <yan.latyshev@gmail.com>
2023-03-27 16:50:43 +0300

    connect firebase , add some crud operations with forms in form.io

--- src/app/main/formio/Formio.jsx
+++ src/app/main/formio/Formio.jsx
@@ -1,12 +1,17 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Form } from "@formio/react";
-import { Button } from "@mui/material";
+import Button from "@mui/material/Button";
 import { useDispatch, useSelector } from "react-redux";
 import { addNewFormData, selectFormsData } from "app/store/dataSlice";
 import { FormBuilder } from "@formio/react";
+import { uid } from "uid";
+import { set, ref, onValue } from "firebase/database";
+import { db } from "../../../firebase";
 
 const Formio = () => {
   const [jsonSchema, setSchema] = useState({ display: "form", components: [] });
+  const [formVisible, setFormVisible] = useState(false);
+  const [forms, setForms] = useState([]);
   const [test, setTest] = useState({
     display: "form",
     components: [
@@ -29,24 +34,42 @@ const Formio = () => {
   });
   const dispatch = useDispatch();
   const fromForm = useSelector(selectFormsData);
-  console.log("fromFormaaaa", fromForm);
+  console.log(fromForm);
+
+  useEffect(() => {
+    onValue(ref(db), (snapshot) => {
+      setForms([]);
+      const data = snapshot.val();
+      if (data !== null) {
+        Object.values(data).map((form) => {
+          setForms((oldArray) => [...oldArray, form]);
+        });
+      }
+    });
+    console.log("effect work now ");
+  }, [jsonSchema]);
 
   const handleSchemaChange = (schema) => {
     setSchema(schema);
     console.log("schema", schema);
-    // console.log("fromForm", fromForm);
+    console.log("fromForm", fromForm);
   };
 
   const handleSubmitNewForm = () => {
+    const uuid = uid();
     dispatch(addNewFormData(jsonSchema.components));
+    set(ref(db, `/${uuid}`), {
+      ...jsonSchema.components,
+    });
+
     setSchema({ display: "form", components: [] });
   };
-
+  console.log("forms", forms);
   return (
-    <div class="w-full border-2 border-black p-4">
-      <div class="my-2 flex justify-center ml-15">
+    <div className="w-full border-2 border-black p-4">
+      <div className="my-2 flex justify-center ml-8">
         <Button
-          className="hover:bg-secondary hover:text-white "
+          className="hover:bg-secondary hover:text-white mr-4 "
           onClick={handleSubmitNewForm}
           variant="contained"
           color="secondary"
@@ -54,11 +77,21 @@ const Formio = () => {
         >
           Upload New form
         </Button>
+        <Button
+          className={
+            formVisible ? "hover:bg-gray-400 text-white bg-gray-700" : "hover:bg-blue-700 text-white bg-blue-400 "
+          }
+          onClick={() => setFormVisible(!formVisible)}
+          variant="contained"
+          size="small"
+        >
+          {formVisible ? "Hide form" : "See all forms"}
+        </Button>
       </div>
       <FormBuilder form={jsonSchema} onChange={handleSchemaChange} />
-      {fromForm.length ? (
-        fromForm.map((form, i) => (
-          <div className="mt-6 w-full">
+      {forms.length && formVisible ? (
+        forms.map((form, i) => (
+          <div key={i} className="mt-6 w-full">
             <div className="w-full h-1 bg-gray-400"></div>
             <h3 className="text-center">{`New Form #${i + 1}`}</h3>
             <Form form={{ components: form }} />
